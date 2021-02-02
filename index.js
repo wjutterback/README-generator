f9adda40e5256000917cb7cd0a7c5261a64648e9
Will Utterback <uwill08@gmail.com>
2021-02-02 13:45:05 -0600

    Created necessary files and packages, implemented all required imports thus far

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generate = require('./assets/generateMarkdown'); // generate will run generateMarkdown function
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
