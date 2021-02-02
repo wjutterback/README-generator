32105c9370471e5a11fec682f47e83ffda22873c
Will Utterback <uwill08@gmail.com>
2021-02-02 15:23:16 -0600

    xmlhttprequest work and GitHub API completed

--- assets/generateMarkdown.js
+++ assets/generateMarkdown.js
@@ -1,6 +1,24 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+const xhr = new XMLHttpRequest();
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  var licenseParse = license.toLowerCase();
+  xhr.open('GET', 'https://api.github.com/licenses/' + licenseParse, true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+      } else {
+        console.error(xhr.statusText);
+      }
+    }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send();
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,6 +30,9 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  renderLicenseBadge(data);
+  // renderLicenseLink(data);
+  // renderLicenseSection(data);
   return `# ${data.title}
 `;
 }
--- index.js
+++ index.js
@@ -4,54 +4,54 @@ const fs = require('fs');
 const generate = require('./assets/generateMarkdown'); // generate will run generateMarkdown function
 
 // TODO: Create an array of questions for user input
-const licenseChoices = ['MIT', 'GPLv2', 'Apache', 'BSD 3-clause'];
+const licenseChoices = ['MIT', 'GPL-2.0', 'Apache-2.0', 'BSD-3-clause'];
 const questions = [
   {
     type: 'input',
     name: 'title',
     message: 'What is your project title?',
   },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Please describe your project',
-  },
-  {
-    type: 'input',
-    name: 'install',
-    message: 'How do you install it?',
-  },
-  {
-    type: 'input',
-    name: 'issues',
-    message: 'How do you report issues?',
-  },
-  {
-    type: 'input',
-    name: 'contribute',
-    message: 'How would you like others to contribue?',
-  },
-  {
-    type: 'input',
-    name: 'test',
-    message: 'What test instructions do you want to provide?',
-  },
+  // {
+  //   type: 'input',
+  //   name: 'description',
+  //   message: 'Please describe your project',
+  // },
+  // {
+  //   type: 'input',
+  //   name: 'install',
+  //   message: 'How do you install it?',
+  // },
+  // {
+  //   type: 'input',
+  //   name: 'issues',
+  //   message: 'How do you report issues?',
+  // },
+  // {
+  //   type: 'input',
+  //   name: 'contribute',
+  //   message: 'How would you like others to contribue?',
+  // },
+  // {
+  //   type: 'input',
+  //   name: 'test',
+  //   message: 'What test instructions do you want to provide?',
+  // },
   {
     type: 'list',
     name: 'license',
     message: 'Which license would you like to use?',
     choices: licenseChoices,
   },
-  {
-    type: 'input',
-    name: 'GitHub',
-    message: 'What is your GitHub username?',
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?',
-  },
+  // {
+  //   type: 'input',
+  //   name: 'GitHub',
+  //   message: 'What is your GitHub username?',
+  // },
+  // {
+  //   type: 'input',
+  //   name: 'email',
+  //   message: 'What is your email address?',
+  // },
 ];
 
 // TODO: Create a function to write README file
@@ -62,7 +62,7 @@ function init() {
   inquirer
     .prompt(questions)
     .then((data) => {
-      console.log(data);
+      generate(data.license);
     })
     .catch((error) => {
       console.log(error);
