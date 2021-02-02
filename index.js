8978b905606991eb296b7062eaab1bb228b48541
Will Utterback <uwill08@gmail.com>
2021-02-02 14:23:50 -0600

    Completed questions portion of inquirer prompt

--- index.js
+++ index.js
@@ -3,9 +3,46 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generate = require('./assets/generateMarkdown'); // generate will run generateMarkdown function
 
-
 // TODO: Create an array of questions for user input
-const questions = [];
+const licenseChoices = ['MIT', 'GPLv2', 'Apache', 'BSD 3-clause'];
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please describe your project',
+  },
+  {
+    type: 'input',
+    name: 'install',
+    message: 'How do you install it?',
+  },
+  {
+    type: 'input',
+    name: 'issues',
+    message: 'How do you report issues?',
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'How would you like others to contribue?',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'What test instructions do you want to provide?',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Which license would you like to use?',
+    choices: licenseChoices,
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
